36020bf94b15694328f8210d0fdc85fcdd0ba520
Atlante <108891261+AtlanteFr@users.noreply.github.com>
2023-07-10 11:58:45 +0200

    Delete frost_land directory

--- frost_land/init.lua
+++
@@ -1,6 +0,0 @@
-local default_path = minetest.get_modpath("frost_land")
-
-dofile(default_path.."/nodes.lua")
-dofile(default_path.."/mapgen.lua")
-dofile(default_path.."/moreblocks.lua")
-dofile(default_path.."/crafting.lua")
